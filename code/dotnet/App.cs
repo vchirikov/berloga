f8da98a5ae45468002faaadbd269a3fc9a780c88
vchirikov <vovanchig@gmail.com>
2025-09-10 03:23:38 +0300

    refactor: move aspnetcore closer to go/gin implementation

--- code/dotnet/App.cs
+++ code/dotnet/App.cs
@@ -1,14 +1,16 @@
 using System.Globalization;
 using System.Net;
 using System.Runtime;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 // enable LOH compaction during GC gen2
 GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
 
-var builder = WebApplication.CreateEmptyBuilder(new() {
+WebApplicationBuilder builder = WebApplication.CreateEmptyBuilder(new() {
   Args = args,
   ApplicationName = "dotnet",
   ContentRootPath = AppContext.BaseDirectory,
@@ -57,16 +59,62 @@ builder.Services.Configure<RouteOptions>(opt => {
   opt.SuppressCheckForUnhandledSecurityMetadata = true;
 });
 
-builder.Services.ConfigureHttpJsonOptions(options => {
-  options.SerializerOptions.TypeInfoResolverChain.Insert(0, JsonContext.Default);
-});
+// cuz we don't use parameter/output binding (to be closer to go/gin) we don't need this:
+// builder.Services.ConfigureHttpJsonOptions(options => {
+//   options.SerializerOptions.TypeInfoResolverChain.Insert(0, JsonContext.Default);
+// });
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 app.UseRouting();
 
-app.MapPost("/{id}", ([FromRoute] int id, [FromBody] JsonBody body) => {
-  return TypedResults.Ok(body.Value + id);
+/*
+ * by [http spec](https://tools.ietf.org/html/rfc7230#section-3.3.3)
+ * All HTTP/1.1 requests should have Transfer-Encoding or Content-Length.
+ * If you don't specify Content-Length Kestrel will use `Transfer-Encoding: chunked`
+ * which produces more bytes and might be less performant (vs gin's constant in Content-Length)
+ * so we must workarounds like an additional middleware with something like:
+ *   await next(context);
+ *   context.Response.Headers.ContentLength = context.Response.Body.Length;
+ * or do serialization/output binding by self. Go/gin can't do mapping so it will be
+ * fair to do it manually too, so instead of this:
+ *  app.MapPost("/{id}", ([FromRoute] int id, [FromBody] JsonBody body) => TypedResults.Ok(body.Value + id));
+ * we will write a bit more code that looks like go approach (but we still use routing)
+ */
+
+app.MapPost("/{id}", async context => {
+  context.Features.Get<IHttpResponseBodyFeature>()!.DisableBuffering();
+  context.Response.ContentType = "application/json; charset=utf-8";
+
+  if (!int.TryParse(
+        (string)context.Request.RouteValues["id"]!,
+        NumberStyles.None,
+        CultureInfo.InvariantCulture,
+        out int id)) {
+    context.Response.StatusCode = 400;
+    await context.Response.WriteAsync( /*lang=json,strict*/ """{"error":"Invalid id"}""",
+      Encoding.UTF8,
+      context.RequestAborted).ConfigureAwait(false);
+    return;
+  }
+
+  string result;
+  try {
+    JsonBody json = await context.Request.ReadFromJsonAsync(JsonContext.Default.JsonBody, context.RequestAborted)
+      .ConfigureAwait(false);
+    result = (id + json.Value).ToString();
+  }
+  catch (JsonException) {
+    context.Response.StatusCode = 400;
+    await context.Response.WriteAsync( /*lang=json,strict*/ """{"error":"Invalid body"}""",
+      Encoding.UTF8,
+      context.RequestAborted).ConfigureAwait(false);
+    return;
+  }
+
+  context.Response.StatusCode = 200;
+  context.Response.Headers.ContentLength = result.Length;
+  await context.Response.WriteAsync(result, Encoding.UTF8, context.RequestAborted).ConfigureAwait(false);
 });
 
 await app.RunAsync().ConfigureAwait(false);
@@ -81,6 +129,9 @@ internal record struct JsonBody
   NewLine = "\n",
   NumberHandling = JsonNumberHandling.Strict,
   DefaultBufferSize = 256,
+  DictionaryKeyPolicy = JsonKnownNamingPolicy.Unspecified,
+  IncludeFields = false,
+  PropertyNameCaseInsensitive = true,
   IndentSize = 2,
   DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
   RespectNullableAnnotations = true,
@@ -104,6 +155,7 @@ internal static class Extensions
     if (bool.TryParse(val, out bool result)) {
       return result;
     }
+
     return val.Length == 1 ? val[0] switch {
       '1' => true,
       '0' => false,
